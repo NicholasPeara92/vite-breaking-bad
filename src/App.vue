f1b3a9c7557a6a1cc1b6309944ccd44ffda5748a
Nicholas Peara <110972835+NicholasPeara92@users.noreply.github.com>
2022-11-28 15:13:13 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import PageTitle from "./components/PageTitle.vue";
+
+export default {
+  components: {
+    PageTitle,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <PageTitle />
+  </div>
+</template>
+
+<style scoped>
+@import "./style/global.scss";
+</style>
